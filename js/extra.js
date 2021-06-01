de49d102862a9b4c2b406ac707af9244800b053b
Magoh <margarita@ulama.co>
2021-06-01 16:27:51 -0500

    avance 3

---
+++ js/extra.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	});
+	$(document).ready(function(){
+	$('.parallax').parallax();
+  });
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+$(document).ready(function(){
+    $('.sidenav').sidenav({
+    	 edge: 'right',
+    	 dragable:'true',
+    });
+  });
+ $(document).ready(function(){
+    $('.modal').modal();
+  });
